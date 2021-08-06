2a6517ac75f632174ac4f94b43bfad2d2c39c344
CeoNabux <lualreye@gmail.com>
2021-08-06 12:48:11 -0500

    header declared and initialized

---
+++ components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-primary">
+    <div class="container max-w-screen-2xl">
+      <div class="flex justify-between items-center">
+        <figure class="w-16 flex justify-center items-center"><img src="" alt=""></figure>
+        <ul class="flex justify-center items-center flex-1">
+          <li class="text-secondary">Inicio</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
