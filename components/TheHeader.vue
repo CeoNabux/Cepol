e4897d433bf392758ad26470b3d2260583bf95ae
CeoNabux <lualreye@gmail.com>
2021-08-06 15:59:27 -0500

    buttons template fixed

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="bg-primary">
+  <div class="bg-white border-b border-primary">
     <div class="container max-w-screen-2xl mx-auto px-2 py-4">
       <div class="flex justify-between items-center">
-        <figure class="w-16 flex justify-center items-center">
-          <img src="@/static/images/imagotipo-color.png" alt="cepol-logo" />
+        <figure class="w-24 flex justify-center items-center">
+          <img src="@/static/images/logo-color.png" alt="cepol-logo" />
         </figure>
-        <ul class="flex justify-center items-center flex-1">
-          <li class="text-lg font-semibold px-1 mx-2 text-white">Inicio</li>
-          <li class="text-lg font-semibold px-1 mx-2 text-white">Servicios</li>
-          <li class="text-lg font-semibold px-1 mx-2 text-white">Blog</li>
-          <li class="text-lg font-semibold px-1 mx-2 text-white">Contáctos</li>
+        <ul class="flex justify-between items-center w-1/3">
+          <li class="text-xl font-semibold py-2 px-1 mx-2 text-primary rounded">Inicio</li>
+          <li class="text-xl font-semibold py-2 px-1 mx-2 text-primary rounded">Servicios</li>
+          <li class="text-xl font-semibold py-2 px-1 mx-2 text-primary rounded">Blog</li>
+          <li class="text-xl font-semibold py-2 px-1 mx-2 text-primary rounded">Contáctos</li>
         </ul>
-        <div class="flex justify-between w-12 items-center">
-          <c-button name="Hola" class="bg-secondary" />
-          <c-button name="Hola" class="bg-secondary" />
+        <div class="flex justify-between items-center w-96">
+          <c-button name="Inicia Sesión" class="border-2 border-secondary text-secondary mx-2" />
+          <c-button name="Registro" class="bg-secondary" />
         </div>
       </div>
     </div>
--- components/global/CButton.vue
+++ components/global/CButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="w-full text-white text-xl rounded-xl font-semibold">
+  <button class="w-full text-white text-lg rounded-xl font-semibold py-2">
     {{name}}
   </button>
 </template>
