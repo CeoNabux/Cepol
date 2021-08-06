824048881edfca4db6070a52799855224ccc6f8d
CeoNabux <lualreye@gmail.com>
2021-08-06 13:21:51 -0500

    cepol colors done and material design icons added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <Tutorial/>
+  <div>
+    <the-header />
+  </div>
 </template>
 
-<script>
-export default {}
-</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#013F66',
+        secondary: '#6da548',
+      },
+    },
   },
   variants: {
     extend: {},
