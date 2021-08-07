1a13414b10818fd8c210f1fe1096e02bfa6c09d7
CeoNabux <lualreye@gmail.com>
2021-08-06 22:28:18 -0500

    header done, preparing menu for resposive

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white border-b border-primary">
+  <div class="bg-white border-b border-secondary">
     <div class="container max-w-screen-2xl mx-auto px-2 py-4">
       <div class="flex justify-between items-center">
         <figure class="w-24 flex justify-center items-center">
@@ -7,24 +7,29 @@
             <img src="@/static/images/logo-color.png" alt="cepol-logo" />
           </a>
         </figure>
-        <ul class="flex justify-between items-center w-1/3">
+        <ul class="hidden lg:flex justify-between items-center w-1/3">
           <li v-for="(item, i) in menu" :key="i" class="text-lg font-medium py-2 px-1 mx-2 text-primary rounded">
             <a :href="item.link">{{ item.name }}</a>
           </li>
         </ul>
-        <div class="flex justify-between items-center w-64">
+        <div class="hidden lg:flex justify-between items-center w-64">
           <div class="px-1 w-1/2 flex justify-center items-center">
             <c-button
               name="Inicia Sesión"
-              class="border-2 border-secondary text-secondary"
+              class="border-2 border-primary text-primary"
             />
           </div>
           <div class="px-1 w-1/2 flex justify-center items-center">
             <c-button
               name="Registro"
-              class="bg-secondary text-white"
+              class="bg-primary text-white"
             />
           </div>
+        </div>
+        <div class="flex lg:hidden w-12 justify-center items-center">
+          <button class="rounded-full bg-primary p-2 shadow-lg" @click="getMenu" v-on-clickaway="closeMenu">
+            <c-icon name="menu" class="text-white" />
+          </button>
         </div>
       </div>
     </div>
@@ -32,7 +37,10 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+import CIcon from './global/CIcon.vue'
 export default {
+  components: { CIcon },
   data: () => ({
     menu: [
       {name: 'Inicio', link: '/'},
@@ -40,6 +48,22 @@ export default {
       {name: 'Servicios', link: '/services'},
       {name: 'Contáctos', link: '/contacts'},
     ]
-  })
+  }),
+  computed: {
+    ...mapGetters('config_drawer', ['showMenu'])
+  },
+  methods: {
+    ...mapActions('config_drawer', ['activeMenu']),
+    getMenu() {
+      if (this.showMenu) {
+        this.activeMenu(false)
+      } else {
+        this.activeMenu(true)
+      }
+    },
+    closeMenu() {
+      this.activeMenu(false)
+    }
+  }
 }
 </script>
---
+++ store/config_drawer.js
@@ -0,0 +1,21 @@
+export const state = () => ({
+  show: false
+})
+
+export const getters = {
+  showMenu (state) {
+    return state.show
+  }
+}
+
+export const mutations = {
+  SHOW_MENU ( state, bool ) {
+    return state.show = bool
+  }
+}
+
+export const actions = {
+  activeMenu({ commit }, bool) {
+    return commit('SHOW_MENU', bool)
+  }
+}
