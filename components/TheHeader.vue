8ff3030da2738f34745c9c4cdf38f6f66810ab76
CeoNabux <lualreye@gmail.com>
2021-08-06 22:34:21 -0500

    menu added for responsive

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white border-b border-secondary">
+  <div class="bg-white border-b border-secondary relative">
     <div class="container max-w-screen-2xl mx-auto px-2 py-4">
       <div class="flex justify-between items-center">
         <figure class="w-24 flex justify-center items-center">
@@ -8,7 +8,11 @@
           </a>
         </figure>
         <ul class="hidden lg:flex justify-between items-center w-1/3">
-          <li v-for="(item, i) in menu" :key="i" class="text-lg font-medium py-2 px-1 mx-2 text-primary rounded">
+          <li
+            v-for="(item, i) in menu"
+            :key="i"
+            class="text-lg font-medium py-2 px-1 mx-2 text-primary rounded"
+          >
             <a :href="item.link">{{ item.name }}</a>
           </li>
         </ul>
@@ -20,14 +24,43 @@
             />
           </div>
           <div class="px-1 w-1/2 flex justify-center items-center">
-            <c-button
-              name="Registro"
-              class="bg-primary text-white"
-            />
+            <c-button name="Registro" class="bg-primary text-white" />
           </div>
         </div>
         <div class="flex lg:hidden w-12 justify-center items-center">
-          <button class="rounded-full bg-primary p-2 shadow-lg" @click="getMenu" v-on-clickaway="closeMenu">
+          <button
+            class="rounded-full bg-primary p-2 shadow-lg"
+            @click="getMenu"
+            v-on-clickaway="closeMenu"
+          >
+            <c-icon name="menu" class="text-white" />
+          </button>
+        </div>
+      </div>
+    </div>
+    <div
+      class="
+        fixed
+        top-0
+        left-0
+        right-0
+        bottom-0
+        bg-primary
+        w-full
+        h-full
+        flex
+        justify-center
+        items
+        center
+      "
+    >
+      <div class="w-full h-full flex flex-col justify-center">
+        <div class="flex w-12 justify-center items-center">
+          <button
+            class="rounded-full bg-primary p-2 shadow-lg"
+            @click="getMenu"
+            v-on-clickaway="closeMenu"
+          >
             <c-icon name="menu" class="text-white" />
           </button>
         </div>
@@ -43,14 +76,14 @@ export default {
   components: { CIcon },
   data: () => ({
     menu: [
-      {name: 'Inicio', link: '/'},
-      {name: 'Blog', link: '/blog'},
-      {name: 'Servicios', link: '/services'},
-      {name: 'Contáctos', link: '/contacts'},
-    ]
+      { name: 'Inicio', link: '/' },
+      { name: 'Blog', link: '/blog' },
+      { name: 'Servicios', link: '/services' },
+      { name: 'Contáctos', link: '/contacts' },
+    ],
   }),
   computed: {
-    ...mapGetters('config_drawer', ['showMenu'])
+    ...mapGetters('config_drawer', ['showMenu']),
   },
   methods: {
     ...mapActions('config_drawer', ['activeMenu']),
@@ -63,7 +96,7 @@ export default {
     },
     closeMenu() {
       this.activeMenu(false)
-    }
-  }
+    },
+  },
 }
 </script>
