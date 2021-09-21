aec28571de30fc39485febdac322f2631327defe
CeoNabux <lualreye@gmail.com>
2021-09-20 20:04:36 -0500

    user registered pass to state

--- store/fireAuthentication.js
+++ store/fireAuthentication.js
@@ -1,10 +1,14 @@
 import { fireAuth } from "~/plugins/firebase/app"
 import { createUserWithEmailAndPassword } from "@firebase/auth"
 export const data = () => ({
-  user: {
-    id: '',
-  }
+  user: null
 })
+
+export const getters = {
+  user (state) {
+    return state.user
+  }
+}
 
 export const mutations = {
   SET_USER(state, payload) {
@@ -14,17 +18,17 @@ export const mutations = {
 
 export const actions = {
   async signUserUp ({ commit }, payload) {
-    const auth = fireAuth
-    const user = await createUserWithEmailAndPassword(auth, payload.email, payload.password)
-      .then(user => {
-        const newUser = {
-          id: user.uid,
-          registeredNotes: []
-        }
-        commit('SET_USER', newUser)
-      })
-      .catch(error => {
-        console.error(error)
-      })
+    try {
+      const auth = fireAuth
+      const user = await createUserWithEmailAndPassword(auth, payload.email, payload.password)
+      const newUser = {
+        id: user.user.uid,
+        registeredNotes:[]
+      }
+      commit('SET_USER', newUser)
+    }
+    catch {
+      console.error(error)
+    }
   }
 }
