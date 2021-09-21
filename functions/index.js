c0f1f5744bf85ad85daeade72e2dc9f5a3fa0b4f
CeoNabux <lualreye@gmail.com>
2021-09-21 12:32:58 -0500

    admin user register added to functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,26 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp()
+
+const db = admin.firestore()
+
+
+exports.AddUserRole = functions.auth.user().onCreate(async (authUser) => {
+  if (authUser.email) {
+    const customClaims = {
+      admin: true
+    }
+    try {
+      var _ = await admin.auth().setCustomUserClaims(authUser.uid, customClaims)
+      return db.collection('roles').doc(authUser.uid).set({
+        email: authUser.email,
+        role: customClaims
+      })
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+})
