71417d67ad9b3a4b59b5773ab207e960ebfe6964
CeoNabux <lualreye@gmail.com>
2021-09-23 14:35:22 -0500

    alert component created and prepared for sign up errors

---
+++ components/global/Alert.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="w-full border border-pink-700 bg-pink-500 py-2 px-3" @click="onClose">
+    <p class="text-sm font-semibold text-pink-700">
+      Oooops ha ocurrido un error
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClose() {
+      this.$emit('dismissed')
+    }
+  }
+}
+</script>
