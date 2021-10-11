c2a489b9b5d60573249e6132f5bb995963f9b79b
CeoNabux <lualreye@gmail.com>
2021-10-11 04:08:43 -0500

    preparing to create courses section

--- components/index/TheServices.vue
+++ components/index/TheServices.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <section class="relative w-full">
-      <div class="w-full h-full relative z-0">
-        <div class="donut absolute top-3 left-6" />
-        <div class="circle absolute bottom-3 right-6" />
-        <div class="triangle-left absolute right-10 top-16" />
-      </div>
+      <div class="donut absolute top-3 left-6" />
+      <div class="circle absolute bottom-3 left-6" />
+      <div class="triangle-left absolute right-10 top-16" />
+
       <div class="w-full pt-10">
-        <h2 class="mb-6 text-gray-800 text-center font-semibold text-4xl">
+        <h2 class="mb-12 text-gray-800 text-center font-semibold text-4xl">
           Modalidades de aprendizaje
         </h2>
         <div class="w-full px-4">
@@ -19,11 +18,16 @@
                 Clases con horario establecido
               </p>
               <div class="w-full lg:w-2/3">
-                <c-button name="Saber por clases síncronas" class="bg-primary" />
+                <c-button
+                  name="Saber por clases síncronas"
+                  class="bg-primary"
+                />
               </div>
             </div>
             <!-- SECCION DE ILUSTRACIONES DE CLASES PRESENCIALES -->
-            <div class="w-full lg:w-2/5 flex flex-wrap justify-evenly items-center">
+            <div
+              class="w-full lg:w-2/5 flex flex-wrap justify-evenly items-center"
+            >
               <div class="w-48">
                 <figure
                   class="
@@ -65,7 +69,7 @@
                     class="h-3/4 mx-auto"
                     alt="clases-zoom"
                   />
-                  <div class="w-full ">
+                  <div class="w-full">
                     <p class="text-base font-semibold text-center">
                       Clases virtuales
                     </p>
@@ -93,11 +97,23 @@
                 Clases cuando quieras, donde quieras
               </p>
               <div class="w-full lg:w-2/3">
-                <c-button name="Saber por clases asíncronas" class="bg-primary" />
+                <c-button
+                  name="Saber por clases asíncronas"
+                  class="bg-primary"
+                />
               </div>
             </div>
             <!-- SECCION DE ILUSTRACIONES EN CLASES EN LINEA -->
-            <div class="w-full lg:w-2/5 flex justify-center lg:justify-start items-center">
+            <div
+              class="
+                w-full
+                lg:w-2/5
+                flex
+                justify-center
+                lg:justify-start
+                items-center
+              "
+            >
               <div class="w-48 ml-3">
                 <figure
                   class="
@@ -134,20 +150,20 @@
 .donut {
   border: 2px solid #013f66;
   border-radius: 100%;
-  width: 12px;
-  height: 12px;
+  width: 56px;
+  height: 56px;
 }
 .circle {
   border-radius: 100%;
-  width: 12px;
-  height: 12px;
+  width: 96px;
+  height: 96px;
   background-color: #013f66;
 }
 .triangle-left {
   width: 0;
   height: 0;
-  border-top: 12px solid transparent;
-  border-right: 24px solid #013f66;
-  border-bottom: 12px solid transparent;
+  border-top: 48px solid transparent;
+  border-right: 96px solid #013f66;
+  border-bottom: 48px solid transparent;
 }
 </style>
