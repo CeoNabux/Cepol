b6dada4fbd7dac829ec5d16dcc92913d9d759fce
CeoNabux <lualreye@gmail.com>
2021-10-19 15:44:24 -0500

    copy added to services

---
+++ pages/services.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="w-full max-w-screen-2xl mx-auto">
+    <!-- PRIMERA SECCION -->
+    <div
+      class="
+        w-full
+        px-4
+        mt-6
+        mx-auto
+        flex flex-wrap
+        justify-center
+        items-center
+      "
+    >
+      <div class="w-full lg:w-1/2">
+        <h2 class="text-primary text-3xl font-semibold mb-4">
+          Examen transformar
+        </h2>
+        <p class="text-lg text-gray-700 font-medium">
+          Preparación para el examen de ingreso a la universidad obligatorio
+          para estudiantes de tercero bachillerato. <br />
+          Este curso incluye todas las herramientas para resolver con agilidad y
+          rapidez todos los ejercicios del examen en los cuatro razonamientos:
+          Numérico, Verbal, Abstracto, Atención y Concentración
+        </p>
+      </div>
+      <div class="mt-2 lg:mt-0 w-full lg:w-1/2">
+        <figure class="w-full flex justify-center items-center">
+          <img
+            src="@/static/images/illustrations/university.png"
+            alt="cepol-image"
+          />
+        </figure>
+      </div>
+    </div>
+    <!-- SEGUNDA SECCION DE SERVICIOS -->
+    <div
+      class="
+        w-full
+        px-4
+        mt-12
+        mx-auto
+        flex flex-row-reverse flex-wrap
+        justify-center
+        items-center
+      "
+    >
+      <div class="w-full lg:w-1/2">
+        <h2 class="text-primary text-3xl font-semibold mb-4">
+          Preuniversitario
+        </h2>
+        <p class="text-lg text-gray-700 font-medium">
+          Dirigido a aspirantes a universidades politécnicas, Fuerzas Armadas y
+          Marina; con capacitación a nivel politécnico en materias como:
+          Matemàticas, Fìsica y Quìmica. Incluye acompañamiento gratuito de
+          apoyo en tus materias durante tu preuniversitario
+        </p>
+      </div>
+      <div class="mt-2 lg:mt-0 w-full lg:w-1/2">
+        <figure class="w-full flex justify-center items-center">
+          <img
+            src="@/static/images/illustrations/university.png"
+            alt="cepol-image"
+          />
+        </figure>
+      </div>
+    </div>
+    <!-- TERCERA SECCION DE SERVICIOS -->
+    <div class="w-full px-4 mt-12">
+      <div class="mt-2 w-full">
+        <figure class="w-full flex justify-center items-center">
+          <img
+            src="@/static/images/illustrations/university.png"
+            alt="cepol-image"
+          />
+        </figure>
+      </div>
+      <div
+        class="
+          w-full
+          lg:w-1/2
+          flex flex-col
+          justify-center
+          items-center
+          mx-auto
+        "
+      >
+        <h2 class="text-primary text-3xl font-semibold mt-2 mb-4">
+          Escuelas de tecnologías
+        </h2>
+        <p class="text-lg text-gray-700 font-medium">
+          El mundo cambio rápido y siempre hay que estar preparados. Por lo que,
+          Cepol está construyendo escuelas de tecnologías para desarrollar tus
+          habilidades con nuestros cursos de: Desarrollo y Pogramación, Diseño
+          Gráfico, Marketing Digital y muchos más.<br />
+          ¡Siempre a la vanguardia del futuro de la tecnología!
+        </p>
+      </div>
+    </div>
+    <!-- CUARTA SECCION DE SERVICIOS -->
+    <div class="w-full px-4 mt-12 mb-6">
+      <div class="mt-2 w-full">
+        <figure class="w-full flex justify-center items-center">
+          <img
+            src="@/static/images/illustrations/university.png"
+            alt="cepol-image"
+          />
+        </figure>
+      </div>
+      <div
+        class="
+          w-full
+          lg:w-1/2
+          flex flex-col
+          justify-center
+          items-center
+          mx-auto
+        "
+      >
+        <h2 class="text-primary text-3xl font-semibold mt-2 mb-4">
+          Orientación Vocacional
+        </h2>
+        <p class="text-lg text-gray-700 font-medium">
+          Cepol te ofrece Test Vocacionales junto a profesionales capacitados
+          para orientarte hacia una mejor elección en tu carrera para la
+          educación superior.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
