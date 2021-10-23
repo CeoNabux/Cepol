af2b684530f760ee1b3e792216ba0cdb79d2ca55
CeoNabux <lualreye@gmail.com>
2021-10-22 23:40:17 -0500

    there need to set configuration in quill editor

--- components/QuillEditor.vue
+++ components/QuillEditor.vue
@@ -8,12 +8,16 @@
 
 <script>
 export default {
-  content: '<p>Estamos trabajando para el editor</p>',
-  modules: {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],
-      ['blockquote', 'code-block']
-    ]
-  }
+  data: () => ({
+    editorOption: {
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],
+          ['blockquote', 'code-block'],
+          ['formula']
+        ]
+      }
+    }
+  })
 }
 </script>
