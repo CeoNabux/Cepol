98a2aa1a1016db11a40b0f2a22aad688f79f6044
CeoNabux <lualreye@gmail.com>
2021-10-25 18:51:14 -0500

    templatete fixed, prepared for blogs

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,29 +1,13 @@
 <template>
   <div class="w-full">
-    <div class="w-full lg:w-1/2 px-2 pt-4">
-      <h1 class="text-gray-700 text-3xl font-medium mb-8">
-        Empecemos a escribir al increíble
-      </h1>
-      <div class="w-full">
-        <quill-editor v-model="editorData" />
-      </div>
-      <div class="w-full lg:w-1/2 mt-12">
-        <c-button name="Guardar" class="bg-secondary" @click="sendData" />
-      </div>
-    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    editorData: '',
-  }),
-  methods: {
-    sendData() {
-      const editorData = this.editorData
-      console.log(editorData)
-    },
-  },
+    
+  })
 }
 </script>
