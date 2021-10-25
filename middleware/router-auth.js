408a8e9e0115551f4c6c2af874fd60761676cf20
CeoNabux <lualreye@gmail.com>
2021-10-25 13:22:00 -0500

    firest middleware added to router to authenticate our user

--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -1,30 +1,13 @@
 export default function ({ store, redirect, error, route }) {
-  // console.log('user', store.state.user)
-  if (!isAuthenticated(store.state.user) && requireAuthentication(route))
+  if (
+    (route.path.includes('student') &&
+      store.state.fireAuthentication.user === null) ||
+    store.state.fireAuthentication.user === undefined
+  ) {
     redirect('/')
-  // /login
-  else if (!isAdmin(store.state.user) && requireAdmin(route))
-    // redirect('/error_admin_only')
-    // redirect('/error', { code: 'require_admin' })
-    error({
-      statusCode: 401,
-      message: 'Only Admin is allowed to access this page',
-    })
+  }
 }
 
-function isAuthenticated(user) {
-  return user
-}
 
-function requireAuthentication(route) {
-  // return !['/', '/about', '/login'].includes(route.path) // || !route.path.startswith('/error_')
-  return ['/console'].includes(route.path)
-}
 
-function isAdmin(user) {
-  return user && user.admin
-}
 
-function requireAdmin(route) {
-  return ['/admin'].includes(route.path)
-}
