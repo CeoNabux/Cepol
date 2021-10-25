474579331172fc324f112739e2e6c4400ad75cb7
CeoNabux <lualreye@gmail.com>
2021-10-25 17:16:06 -0500

    schools section fixed

--- components/index/TheSchools.vue
+++ components/index/TheSchools.vue
@@ -27,34 +27,94 @@
             <div class="grid grid-cols-2 gap-4">
               <div class="flex justify-end items-center">
                 <div
-                  class="bg-white flex flex-col justify-center items-center w-32 h-32 sm:w-60 sm:h-60 rounded-l-3xl rounded-tr-3xl"
+                  class="
+                    bg-white
+                    flex flex-col
+                    justify-center
+                    items-center
+                    w-32
+                    h-32
+                    sm:w-60 sm:h-60
+                    rounded-l-3xl rounded-tr-3xl
+                  "
                 >
-                  <img src="@/static/images/illustrations/development.png" alt="">
-                  <p class="text-secondary text-sm md:text-base">Desarrollo web</p>
+                  <img
+                    src="@/static/images/illustrations/development.png"
+                    alt="cepol-schools"
+                  />
+                  <p class="text-secondary text-sm md:text-base">
+                    Desarrollo web
+                  </p>
                 </div>
               </div>
               <div class="flex justify-start items-end">
                 <div
-                  class="bg-white flex flex-col justify-center items-center w-24 h-24 sm:w-36 sm:h-36 rounded-r-3xl rounded-tl-3xl"
+                  class="
+                    bg-white
+                    flex flex-col
+                    justify-center
+                    items-center
+                    w-24
+                    h-24
+                    sm:w-36 sm:h-36
+                    rounded-r-3xl rounded-tl-3xl
+                  "
                 >
-                  <img src="@/static/images/illustrations/design.png" alt="">
-                  <p class="text-secondary text-sm md:text-base">Diseño gráfico</p>
+                  <img
+                    src="@/static/images/illustrations/design.png"
+                    alt="cepol-schools"
+                  />
+                  <p class="text-secondary text-sm md:text-base">
+                    Diseño gráfico
+                  </p>
                 </div>
               </div>
               <div class="flex justify-end items-center">
                 <div
-                  class="bg-white flex flex-col justify-center items-center w-24 h-24 sm:w-36 sm:h-36 rounded-l-3xl rounded-br-3xl"
+                  class="
+                    bg-white
+                    flex flex-col
+                    justify-center
+                    items-center
+                    w-24
+                    h-24
+                    sm:w-36 sm:h-36
+                    rounded-l-3xl rounded-br-3xl
+                  "
                 >
-                  <img src="@/static/images/illustrations/marketing.png" alt="">
-                  <p class="text-secondary text-sm md:text-base">Growth marketing</p>
+                  <div class="w-9 md:w-full flex justify-center items-center">
+                    <img
+                      src="@/static/images/illustrations/marketing.png"
+                      alt="cepol-schools"
+                    />
+                  </div>
+                  <p class="text-secondary text-xs text-center md:text-base">
+                    Growth marketing
+                  </p>
                 </div>
               </div>
               <div class="flex justify-start items-center">
                 <div
-                  class="bg-white flex flex-col justify-center items-center w-24 h-24 sm:w-36 sm:h-36 rounded-r-3xl rounded-br-3xl"
+                  class="
+                    bg-white
+                    flex flex-col
+                    justify-center
+                    items-center
+                    w-24
+                    h-24
+                    sm:w-36 sm:h-36
+                    rounded-r-3xl rounded-br-3xl
+                  "
                 >
-                  <img src="@/static/images/illustrations/math.png" alt="">
-                  <p class="text-secondary text-sm md:text-base">Formación básica</p>
+                  <div class="w-9 md:w-full flex justify-center items-center">
+                    <img
+                      src="@/static/images/illustrations/math.png"
+                      alt="cepol-schools"
+                    />
+                  </div>
+                  <p class="text-secondary text-xs text-center md:text-base">
+                    Formación básica
+                  </p>
                 </div>
               </div>
             </div>
