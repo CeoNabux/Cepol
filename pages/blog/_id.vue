86fdfebbb66be08588cd31b84eddb786499464e8
CeoNabux <lualreye@gmail.com>
2021-10-27 07:27:04 -0500

    preparing data for index blogs and index id pages

---
+++ pages/blog/_id.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Estamos dentro de un post
+  </div>
+</template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="w-full">
-
+  <div class="bg-gradient-to-tr from-white to-secondary">
+    <div class="w-full max-w-screen-2xl mx-auto">
+      Estamos dentro del index de blogs
+    </div>
   </div>
 </template>
 
