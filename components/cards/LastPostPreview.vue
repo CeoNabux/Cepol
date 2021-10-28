ded211807f0501800adf7b1d789ec5affd5615d1
CeoNabux <lualreye@gmail.com>
2021-10-28 10:29:05 -0500

    blog index rendenring with first component

---
+++ components/cards/LastPostPreview.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="border border-secondary flex justify-center items-center shadow-lg rounded-lg bg-white">
+      <div class="w-full md:w-60 flex justify-center items-center">
+        <img :src="image" alt="last-post-image" class="image-mask" />
+      </div>
+      <div class="w-full md:w-2/3">
+        <p class="text-gray-700 font-semibold text-xl mb-6">
+          {{ title }}
+        </p>
+        <div class="w-32">
+          <c-button name="Leer más" class="bg-yellow-700" @click="redirectioToPost(id)" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    redirectionToPost(id) {
+      this.$router.push(`blog/${id}`)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.mask-image {
+  clip-path: polygon(0 0, 100% 0, 90% 100%, 0% 100%);
+}
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="bg-gradient-to-tr from-white to-secondary">
     <div class="w-full max-w-screen-2xl mx-auto">
-      Estamos dentro del index de blogs
+      <!-- CONTENEDOR PRINCIPAL -->
+      <div v-if="getPublishedPosts.length" class="w-full p-2">
+        <!-- CONTENEDOR DE ULTIMO POST -->
+        <div class="px-2 w-full md:w-1/2 flex justify-center items-center mx-auto">
+          <!-- ULTIMO POST -->
+          <last-post-preview
+            :title="getPublishedPosts[0].title"
+            :image="getPublishedPosts[0].image"
+            :id="getPublishedPosts[0].id"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+import LastPostPreview from '~/components/cards/LastPostPreview.vue'
 export default {
-  
+  components: { LastPostPreview },
+  computed: {
+    ...mapGetters('fireBlogs', ['getPublishedPosts']),
+  },
+  created() {
+    this.renderPosts()
+  },
+  methods: {
+    ...mapActions('fireBlogs', ['fetchPublishedPost']),
+    renderPosts() {
+      if(!this.getPublishedPosts.length) {
+        this.fetchPublishedPost()
+      }
+    }
+  }
 }
 </script>
