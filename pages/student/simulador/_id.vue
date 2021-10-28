f02eff2511120d10982cb8d01ff4e0b1c502346d
CeoNabux <lualreye@gmail.com>
2021-10-28 12:16:27 -0500

    console log remove from simulator

--- pages/student/simulador/_id.vue
+++ pages/student/simulador/_id.vue
@@ -100,7 +100,6 @@ export default {
   mounted() {
     this.validatingTestById()
     this.redirectionToDashboard()
-    console.log('Esto es lo que quiero renderizar', this.getCurrentSimulator)
   },
   methods: {
     ...mapActions('fireSimulator', ['setCurrentSimulator']),
